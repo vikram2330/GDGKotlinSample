bec07e9b656adbcdffe7df739a4a346342e6892a
Kishore Babu <barykaed@gmail.com>
2019-10-10 22:07:04 +0530

    Add: Methods added for more API calls

--- app/src/main/java/com/vikram/gdgkotlinsample/services/ChuckNorrisApiInterface.kt
+++ app/src/main/java/com/vikram/gdgkotlinsample/services/ChuckNorrisApiInterface.kt
@@ -1,11 +1,19 @@
 package com.vikram.gdgkotlinsample.services
 
+import com.vikram.gdgkotlinsample.models.Joke
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Created by Vikram on 2019-10-10.
  */
 interface ChuckNorrisApiInterface {
     @GET("jokes/random")
-    suspend fun getRandomJoke()
+    suspend fun getRandomJoke(): Joke
+
+    @GET("jokes/categories")
+    suspend fun getJokeCategories(): List<String>
+
+    @GET("jokes/random")
+    suspend fun getRandomJokeOfCategory(@Query("category") category: String): Joke
 }
