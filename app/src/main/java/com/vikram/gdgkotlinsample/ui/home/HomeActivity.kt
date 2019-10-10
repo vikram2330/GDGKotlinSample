38085e025692ef9482722212272f9e793669b507
Kishore Babu <barykaed@gmail.com>
2019-10-10 22:21:45 +0530

    Refactor: Different packages for base, and home activities

--- app/src/main/java/com/vikram/gdgkotlinsample/ui/home/HomeActivity.kt
+++ app/src/main/java/com/vikram/gdgkotlinsample/ui/home/HomeActivity.kt
@@ -1,14 +1,15 @@
-package com.vikram.gdgkotlinsample.ui
+package com.vikram.gdgkotlinsample.ui.home
 
 import android.os.Bundle
 import com.vikram.gdgkotlinsample.R
 import com.vikram.gdgkotlinsample.databinding.HomeActivityBinding
+import com.vikram.gdgkotlinsample.ui.base.BaseActivity
 import com.vikram.gdgkotlinsample.viewmodel.HomeViewModel
 
 /**
  * Created by Vikram on 2019-10-10.
  */
-class HomeActivity: BaseActivity<HomeActivityBinding,HomeViewModel>() {
+class HomeActivity: BaseActivity<HomeActivityBinding, HomeViewModel>() {
 
 
     override fun getViewModelClass(): Class<HomeViewModel> = HomeViewModel::class.java
