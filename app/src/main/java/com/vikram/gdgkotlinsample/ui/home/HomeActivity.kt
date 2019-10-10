0449eabe33761b39dbae34fd1f4f722c05f6fd87
Kishore Babu <barykaed@gmail.com>
2019-10-10 22:50:38 +0530

    Add: RecyclerView added for HomeActivity

--- app/src/main/java/com/vikram/gdgkotlinsample/ui/home/HomeActivity.kt
+++ app/src/main/java/com/vikram/gdgkotlinsample/ui/home/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.vikram.gdgkotlinsample.ui.home
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.vikram.gdgkotlinsample.R
 import com.vikram.gdgkotlinsample.databinding.HomeActivityBinding
 import com.vikram.gdgkotlinsample.ui.base.BaseActivity
@@ -9,15 +11,21 @@ import com.vikram.gdgkotlinsample.viewmodel.HomeViewModel
 /**
  * Created by Vikram on 2019-10-10.
  */
-class HomeActivity: BaseActivity<HomeActivityBinding, HomeViewModel>() {
-
-
+class HomeActivity : BaseActivity<HomeActivityBinding, HomeViewModel>(),
+    JokeCategoriesAdapter.InteractionListener {
     override fun getViewModelClass(): Class<HomeViewModel> = HomeViewModel::class.java
     override fun layoutId(): Int = R.layout.home_activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        with(binding.rvJokeCategories) {
+            layoutManager = LinearLayoutManager(this@HomeActivity, RecyclerView.VERTICAL, false)
+            adapter = JokeCategoriesAdapter(this@HomeActivity)
+        }
     }
 
+    override fun onItemSelected(position: Int, item: String) {
 
+    }
 }
